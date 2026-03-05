ab55bdcb319626ea8026dce518f80f7a33a35f7b
Lily Gross <lilybethgross@gmail.com>
2026-03-05 08:24:16 -0800

    Update CI workflow and add Terraform files

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    render = {
+        source = "renderinc/render"
+        version = "1.8.0"
+    }
+  }
+}
+
+provider "render" {
+    api_key = var.api_key
+}
+
+resource "render_service" "web_app" {
+    name = "ci-demo-app"
+    type = "web_service"
+    repo = "https://github.com/AppalachianMounta1n/devops-pipeline"
+    env = "docker"
+    plan = "starter"
+    branch = "main"
+    build_command = "docker build -t app ."
+    start_command = "docker run -p 3000:3000 app"
+    auto_deploy = true
+}
